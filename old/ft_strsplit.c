168d4ef3115b0244bb892634faae15e3ec64aff6
Iwantaponey <juju0397@msn.com>
2019-01-03 22:36:22 +0100

    Djd
    Djdjdjj

--- old/ft_strsplit.c
+++ old/ft_strsplit.c
@@ -1,4 +1,50 @@
 #include <stdlib.h>
+include "libft.h"
+
+static size_t		ft_count_word(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if ((i == 1 && s[i] != c) || (s[i] != c && s[i - 1] == c))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static int			ft_is_begin_word(char const *s, size_t index, char c)
+{
+	if (index == 0 && s[index] != c)
+		return (1);
+	if (s[index] != c && s[index - 1] == c)
+		return (1);
+	return (0);
+}
+
+char				**ft_strsplit(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	**tab;
+
+	i = 0;
+	k = 0;
+	if ((tab = (char **)malloc(sizeof(char *) * ft_count_word(s, c))) == NULL)
+		return (NULL);
+	if (ft_count_word(s, c) == 0)
+		return (tab);
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
 #include <string.h>
 
 int	*how_many_char(char const *str, char c, int nb)
@@ -12,7 +58,7 @@ int	*how_many_char(char const *str, char c, int nb)
 	if (res = malloc(sizeof(int)*nb))
 	{
 		while (str[j] && i < nb)
-		{
+		
 			if (str[j] != c)
 			{
 				++res[i];
