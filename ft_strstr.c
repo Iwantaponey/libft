947710f8bcb66403acb71fe66b6329a0479af7d2
Julie SEGUENI <jsegueni@e1r4p8.42.fr>
2019-01-03 17:31:48 +0100

    wlefjewif

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: jsegueni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 18:14:06 by jsegueni          #+#    #+#             */
-/*   Updated: 2019/01/03 16:58:27 by jsegueni         ###   ########.fr       */
+/*   Updated: 2019/01/03 17:31:37 by jsegueni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 char	*ft_strstr(char *haystack, const char *needle)
 {
-	int		i;
-	int		j;
-	char	*res;
+	int	i;
+	int	j;
+	int	res;
 
-	if (needle == '\0')
-	{
+	if (*needle == '\0')
 		return (haystack);
-	}
 	i = 0;
 	while (haystack[i])
 	{
 		j = 0;
 		while (haystack[i] && needle[j] && haystack[i] == needle[j])
 		{
-			res = haystack + i;
-			++i;
-			++j;
-			if (j == ft_strlen(needle))
+			res = i;
+			while (haystack[i] && needle[j] && haystack[i] == needle[j])
 			{
-				return (res);
+				++i;
+				++j;
+				if (j == ft_strlen(needle))
+					return (haystack + res);
 			}
+			i = res;
 		}
 		++i;
 	}
