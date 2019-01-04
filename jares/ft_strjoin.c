49c412cd1e770f160719c9ee2f6fd182f873cf6c
Julie SEGUENI <jsegueni@e1r11p9.42.fr>
2019-01-04 06:34:09 +0100

    wekfjewif

--- jares/ft_strjoin.c
+++ jares/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jsegueni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 18:48:31 by jsegueni          #+#    #+#             */
-/*   Updated: 2019/01/04 02:41:43 by jsegueni         ###   ########.fr       */
+/*   Updated: 2019/01/04 06:33:19 by jsegueni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	if ((res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 	{
-		if (s1)
-			while (s1[i])
-				res[j++] = s1[i++];
+		while (s1[i])
+			res[j++] = s1[i++];
 		i = 0;
-		if (s2)
-			while (s2[i])
-				res[j++] = s2[i++];
+		while (s2[i])
+			res[j++] = s2[i++];
 		res[j] = '\0';
 		return (res);
 	}
